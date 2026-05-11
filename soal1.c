899b8953de2224d7839cdf79a873318ae4f3f1de
micelele1 <michelleyeo36@gmail.com>
2026-05-11 09:01:48 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -1,5 +1,68 @@
-#include <stdio.h>
+/** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2025/2026
+ *   Modul               : 5 - Foundation of Algorithm
+ *   Hari dan Tanggal    : 11 Mei 2026
+ *   Nama (NIM)          : Michelle (13224098)
+ *   Nama File           : soal2.c
+ *   Deskripsi           : Membaca sebuah peta koneksi ruangan yang tersimpan dalam bentuk adjacency matric,
+ *                          dan menghasilkan ringkasan konektivitas berdasarkan perhitungan derajat setiap simpul,
+ *                          simpul maksimum, dan menentukan apakah terdapat simpul yang terisolasi.
+ */
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stdbool.h>
 
-int main(void) {
-    return 0;
+void readMatrix(int adjmatrix[][], int size){
+    int val;
+    for(int i=0; i<size; i++){
+        for(int j=0; j<size; j++){
+            scanf("%d", &val);
+            adjmatrix[i][j]=val;
+        }
+    }
+}
+
+void degree(int adjmatrix[][], int size, int deg[]){
+    for(int i=0; i<size; i++){
+        deg[i]=0;
+        for(int j=0; j<size; j++){
+            if(adjmatrix[i][j]==1){
+                deg[i]++;
+            }
+        }
+        printf("DEGREE %d %d", &i, &deg[i]);
+    }
+}
+
+int maxVertex(int deg[], int size){
+    int temp=0;
+    for(int i=0; i<size; i++){
+        if(deg[temp]<deg[i]){
+            temp=i;
+        }
+    }
+    printf("MAX_VERTEX %d", &temp);
+}
+void isolasi(int deg[], int size){
+    for(int i=0; i<size; i++){
+        if(deg[i]==0){
+            printf("ISOLATED %d", &i);
+        }
+    }
+
+}
+
+int main(){
+    int i;
+    int size;
+    if (scanf("%d", &size) == 1){
+        int adjmatrix[size][size];
+        readMatrix(adjmatrix, size);
+        int deg[size];
+        degree(adjmatrix, size, deg);
+        maxVertex(deg, size);
+        isolasi(deg, size);
+    }
+
 }
